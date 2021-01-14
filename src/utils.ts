f3744d9c8190543309a7fb505b2d7b3816309b12
Sebestyén Dániel <dannystyleart@gmail.com>
2021-01-15 00:19:38 +0100

    Add ip validation for setIP method

--- src/utils.ts
+++ src/utils.ts
@@ -2,6 +2,12 @@ export const isNonEmptyString = (arg: any): arg is string => typeof arg === 'str
 
 export const isHttpProtocol = (arg: any): boolean => isNonEmptyString(arg) && /^https?:/i.test(arg);
 
+export const isValidIPv4Address = (arg: any): boolean => {
+  if (!isNonEmptyString(arg)) return false;
+  const pattern = /^(([1-9]?[0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]).){3}([1-9]?[0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/;
+  return pattern.test(arg);
+};
+
 export const withDefaults = (object: any, defaults: any) => ({
   ...defaults,
   ...object
