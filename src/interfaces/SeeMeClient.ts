f67478ac9bbd2c21303478c2e3f84a8dc9fa95bd
Sebestyén Dániel <dannystyleart@gmail.com>
2021-01-29 22:54:32 +0100

    feat(sendSMS): Add send sms request

--- src/interfaces/SeeMeClient.ts
+++ src/interfaces/SeeMeClient.ts
@@ -1,5 +1,14 @@
+import { SendMessageCallbackStatus } from '../SeeMeClient';
+
 export interface SeeMeClient {
   getBalance(): Promise<any>;
   setIP(ipAddress: string): Promise<any>;
-  sendMessage(address: string, message: string): Promise<any>;
+  sendSMS(number: string, message: string, optionalParams?: SendMessageOptionalParams): Promise<any>;
+}
+
+export interface SendMessageOptionalParams {
+  sender?: string;
+  referenceId?: string | number;
+  callbackStatus?: 'all' | SendMessageCallbackStatus | Array<SendMessageCallbackStatus>;
+  callbackURL?: string;
 }
